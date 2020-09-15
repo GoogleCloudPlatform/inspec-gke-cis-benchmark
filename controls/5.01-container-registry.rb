05064ed71cc45caf3a82d3feaa80fe1dca875b81
Konrad Schieban <kschieban@google.com>
2020-09-14 22:58:40 -0400

    Added control 5.03 - cluster access for GCR

--- controls/5.01-container-registry.rb
+++ controls/5.01-container-registry.rb
@@ -15,6 +15,7 @@
 title 'Ensure Image Vulnerability Scanning using GCR Container Analysis or a third party provider'
 
 gcp_project_id = input('gcp_project_id')
+gcp_gke_locations = input('gcp_gke_locations')
 cis_version = input('cis_version')
 cis_url = input('cis_url')
 control_id = '5.1'
@@ -26,6 +27,8 @@ registry_storage_object_creator_list = input('registry_storage_object_creator_li
 registry_storage_legacy_bucket_owner_list = input('registry_storage_legacy_bucket_owner_list')
 registry_storage_legacy_bucket_writer_list = input('registry_storage_legacy_bucket_writer_list')
 registry_storage_legacy_object_owner_list = input('registry_storage_legacy_object_owner_list')
+
+gke_clusters = GKECache(project: gcp_project_id, gke_locations: gcp_gke_locations).gke_clusters_cache
 
 # 5.1.1
 sub_control_id = "#{control_id}.1"
@@ -111,6 +114,67 @@ control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
         subject { google_storage_bucket_iam_binding(bucket: "artifacts.#{gcp_project_id}.appspot.com", role: iam_policy.role) }
         it 'match the allow list' do
           expect(subject.members).to cmp(role_member_list)
+        end
+      end
+    end
+  end
+end
+
+# 5.1.3
+sub_control_id = "#{control_id}.3"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'none'
+
+  title "[#{control_abbrev.upcase}] Minimize cluster access to read-only for GCR"
+
+  desc 'Configure the Cluster Service Account with Storage Object Viewer Role to only allow read-only access to GCR.'
+  desc 'rationale', "The Cluster Service Account does not require administrative access to GCR, only requiring
+  pull access to containers to deploy onto GKE. Restricting permissions follows the principles
+  of least privilege and prevents credentials from being abused beyond the required role."
+
+  tag cis_scored: false
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://cloud.google.com/container-registry/docs/container-analysis'
+
+  if gke_clusters.nil? || gke_clusters.count.zero?
+    impact 'none'
+    describe "[#{gcp_project_id}] does not have any GKE clusters, this test is Not Applicable." do
+      skip "[#{gcp_project_id}] does not have any GKE clusters."
+    end
+  elsif google_storage_bucket(name: "artifacts.#{gcp_project_id}.appspot.com").name.nil?
+    impact 'none'
+    describe "[#{gcp_project_id}] does not have a storage bucket for Google Container Registry Images, this test is Not Applicable." do
+      skip "[#{gcp_project_id}] does not have a storage bucket for Google Container Registry Images."
+    end
+  else
+    gke_clusters.each do |gke_cluster|
+      google_container_node_pools(project: gcp_project_id, location: gke_cluster[:location], cluster_name: gke_cluster[:cluster_name]).node_pool_names.each do |nodepoolname|
+        nodepool_sa = google_container_node_pool(project: gcp_project_id, location: gke_cluster[:location], cluster_name: gke_cluster[:cluster_name], nodepool_name: nodepoolname).config.service_account
+
+        # 1st check: Make sure that nodepool service account does not have any role other than storage.objectViewer assigned
+        google_storage_bucket_iam_bindings(bucket: "artifacts.#{gcp_project_id}.appspot.com").iam_binding_roles.each do |iam_binding_role|
+          next if iam_binding_role == 'roles/storage.objectViewer'
+          describe "[#{gcp_project_id}] IAM Binding of role #{iam_binding_role} on Storage Bucket artifacts.#{gcp_project_id}.appspot.com" do
+            subject { google_storage_bucket_iam_binding(bucket: "artifacts.#{gcp_project_id}.appspot.com", role: iam_binding_role) }
+            its('members') { should_not include "serviceAccount:#{nodepool_sa}" }
+          end
+        end
+
+        # 2nd check: Make sure that nodepool service account has role storage.objectViewer assigned
+        iam_binding_object_viewer = google_storage_bucket_iam_binding(bucket: "artifacts.#{gcp_project_id}.appspot.com", role: 'roles/storage.objectViewer')
+        describe "[#{gcp_project_id}] IAM Binding of role roles/storage.objectViewer on Storage Bucket artifacts.#{gcp_project_id}.appspot.com" do
+          subject { iam_binding_object_viewer }
+          it { should exist }
+        end
+        next if iam_binding_object_viewer.members.nil?
+        describe "[#{gcp_project_id}] IAM Binding of role roles/storage.objectViewer on Storage Bucket artifacts.#{gcp_project_id}.appspot.com" do
+          subject { iam_binding_object_viewer }
+          its('members') { should include "serviceAccount:#{nodepool_sa}" }
         end
       end
     end
