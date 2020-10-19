e6242dbb47380a83060c4c72239924366e8d3266
Konrad Schieban <kschieban@google.com>
2020-10-19 16:43:31 -0400

    Breaking Change - Introducing Inspec-k8s and SSH controls (#11)
    * Restructuring of repo and introducing inspec-k8s (#10)
    * added 4.2.2
    * added 4.2.3
    * added 4.2.5
    * added 4.2.6
    * added 4.2.7
    * added 4.2.8
    * added 3.03
    * added 3.04
    * added 3.2.1
    * added 3.2.2
    * added 3.2.3
    * added 3.2.4
    * added 3.2.5
    * adding 3.2.6
    * adding 3.2.7
    * adding 3.2.8
    * adding 3.2.9
    * adding 3.2.10
    * adding 3.2.11
    * adding 3.2.12
    * linting
    * adding wrapper script

---
+++ inspec-gke-cis-ssh/controls/3.02-kubelet.rb
@@ -0,0 +1,379 @@
+# Copyright 2020 The inspec-gke-cis-benchmark Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+gcp_project_id = input('gcp_project_id')
+cis_version = input('cis_version')
+cis_url = input('cis_url')
+control_id = '3.2'
+control_abbrev = 'kubelet'
+
+client_ca_file_path = input('client_ca_file_path')
+event_record_qps = input('event_record_qps')
+tls_cert_file = input('tls_cert_file')
+tls_private_key_file = input('tls_private_key_file')
+
+kubelet_config_file_path = command('ps -ef | grep kubelet | grep -e "--config " | sed "s/^.*\(--config .* \).*$/\1/"  | awk \'{print $2}\'').stdout.split("\n").first
+kubelet_config_file = yaml(kubelet_config_file_path)
+
+# 3.2.1
+sub_control_id = "#{control_id}.1"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --anonymous-auth argument is set to false"
+
+  desc 'Disable anonymous requests to the Kubelet server.'
+  desc 'rationale', "When enabled, requests that are not rejected by other configured authentication methods
+  are treated as anonymous requests. These requests are then served by the Kubelet server.
+  You should rely on authentication to authorize access and disallow anonymous requests."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet-authentication-authorization/#kubelet-authentication'
+
+  anonymous_auth_config = kubelet_config_file.authentication['anonymous']['enabled']
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { anonymous_auth_config }
+    it 'should have anonymous authentication disabled' do
+      expect(subject).to cmp 'false'
+    end
+  end
+end
+
+# 3.2.2
+sub_control_id = "#{control_id}.2"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --authorization-mode argument is not set to AlwaysAllow"
+
+  desc 'Do not allow all requests. Enable explicit authorization.'
+  desc 'rationale', "Kubelets, by default, allow all authenticated requests (even anonymous ones) without
+  needing explicit authorization checks from the apiserver. You should restrict this behavior
+  and only allow explicitly authorized requests."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet-authentication-authorization/#kubelet-authentication'
+
+  authorization_mode_config = kubelet_config_file.authorization['mode']
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { authorization_mode_config }
+    it 'should not have authorization mode set to AlwaysAllow' do
+      expect(subject).not_to cmp 'AlwaysAllow'
+    end
+  end
+end
+
+# 3.2.3
+sub_control_id = "#{control_id}.3"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --client-ca-file argument is set as appropriate"
+
+  desc 'Enable Kubelet authentication using certificates.'
+  desc 'rationale', "The connections from the apiserver to the kubelet are used for fetching logs for pods,
+  attaching (through kubectl) to running pods, and using the kubelet’s port-forwarding
+  functionality. These connections terminate at the kubelet’s HTTPS endpoint. By default, the
+  apiserver does not verify the kubelet’s serving certificate, which makes the connection
+  subject to man-in-the-middle attacks, and unsafe to run over untrusted and/or public
+  networks. Enabling Kubelet certificate authentication ensures that the apiserver could
+  authenticate the Kubelet before submitting any requests."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet-authentication-authorization/#kubelet-authentication'
+
+  client_ca_file = kubelet_config_file.authentication['x509']['clientCAFile']
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { client_ca_file }
+    it 'should be have certificate authentication enabled' do
+      expect(subject).to cmp client_ca_file_path
+    end
+  end
+end
+
+# 3.2.4
+sub_control_id = "#{control_id}.4"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --read-only-port argument is set to 0"
+
+  desc 'Disable the read-only port.'
+  desc 'rationale', "The Kubelet process provides a read-only API in addition to the main Kubelet API.
+  Unauthenticated access is provided to this read-only API which could possibly retrieve
+  potentially sensitive information about the cluster."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/admin/kubelet/'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('readOnlyPort') { should cmp 0 }
+  end
+end
+
+# 3.2.5
+sub_control_id = "#{control_id}.5"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --streaming-connection-idle-timeout argument is not set to 0"
+
+  desc 'Do not disable timeouts on streaming connections.'
+  desc 'rationale', "Setting idle timeouts ensures that you are protected against Denial-of-Service attacks,
+  inactive connections and running out of ephemeral ports."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://github.com/kubernetes/kubernetes/pull/18552'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('streamingConnectionIdleTimeout') { should_not eq 0 }
+  end
+end
+
+# 3.2.6
+sub_control_id = "#{control_id}.6"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --protect-kernel-defaults argument is set to true"
+
+  desc 'Protect tuned kernel parameters from overriding kubelet default kernel parameter values.'
+  desc 'rationale', "Kernel parameters are usually tuned and hardened by the system administrators before
+  putting the systems into production. These parameters protect the kernel and the system.
+  Your kubelet kernel defaults that rely on such parameters should be appropriately set to
+  match the desired secured system state. Ignoring this could potentially lead to running
+  pods with undesired kernel behavior."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('protectKernelDefaults') { should cmp 'true' }
+  end
+end
+
+# 3.2.7
+sub_control_id = "#{control_id}.7"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --make-iptables-util-chains argument is set to true"
+
+  desc 'Allow Kubelet to manage iptables.'
+  desc 'rationale', "Kubelets can automatically manage the required changes to iptables based on how you
+  choose your networking options for the pods. It is recommended to let kubelets manage
+  the changes to iptables. This ensures that the iptables configuration remains in sync with
+  pods networking configuration. Manually configuring iptables with dynamic pod network
+  configuration changes might hamper the communication between pods/containers and to
+  the outside world. You might have iptables rules too restrictive or too open."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('makeIPTablesUtilChains') { should_not cmp 'false' }
+  end
+end
+
+# 3.2.8
+sub_control_id = "#{control_id}.8"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'none'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --hostname-override argument is not set"
+
+  desc 'Do not override node hostnames.'
+  desc 'rationale', "Overriding hostnames could potentially break TLS setup between the kubelet and the
+  apiserver. Additionally, with overridden hostnames, it becomes increasingly difficult to
+  associate logs with a particular node and process them for security analytics. Hence, you
+  should setup your kubelet nodes with resolvable FQDNs and avoid overriding the
+  hostnames with IPs."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://github.com/kubernetes/kubernetes/issues/22063'
+
+  describe "[#{gcp_project_id}] This setting is not configurable via the Kubelet config file, this test is Not Applicable." do
+    skip "[#{gcp_project_id}] This setting is not configurable via the Kubelet config file."
+  end
+end
+
+# 3.2.9
+sub_control_id = "#{control_id}.9"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --event-qps argument is set to 0 or a level which
+  ensures appropriate event capture"
+
+  desc 'Security relevant information should be captured. The --event-qps flag on the Kubelet can
+  be used to limit the rate at which events are gathered. Setting this too low could result in
+  relevant events not being logged, however the unlimited setting of 0 could result in a denial
+  of service on the kubelet.'
+  desc 'rationale', "It is important to capture all events and not restrict event creation. Events are an important
+  source of security information and analytics that ensure that your environment is
+  consistently monitored using the event data."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://github.com/kubernetes/kubernetes/blob/master/pkg/kubelet/apis/kubeletconfig/v1beta1/types.go'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('eventRecordQPS') { should cmp event_record_qps }
+  end
+end
+
+# 3.2.10
+sub_control_id = "#{control_id}.10"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --tls-cert-file and --tls-private-key-file arguments
+  are set as appropriate"
+
+  desc 'Setup TLS connection on the Kubelets.'
+  desc 'rationale', "Kubelet communication contains sensitive parameters that should remain encrypted in
+  transit. Configure the Kubelets to serve only HTTPS traffic."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'http://rootsquash.com/2016/05/10/securing-the-kubernetes-api/'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('tlsCertFile') { should cmp tls_cert_file }
+    its('tlsPrivateKeyFile') { should cmp tls_private_key_file }
+  end
+end
+
+# 3.2.11
+sub_control_id = "#{control_id}.11"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the --rotate-certificates argument is not set to false"
+
+  desc 'Enable kubelet client certificate rotation.'
+  desc 'rationale', "The --rotate-certificates setting causes the kubelet to rotate its client certificates by
+  creating new CSRs as its existing credentials expire. This automated periodic rotation
+  ensures that the there is no downtime due to expired certificates and thus addressing
+  availability in the CIA security triad."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet-tls-bootstrapping/#kubelet-configuration'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('rotateCertificates') { should_not cmp 'false' }
+  end
+end
+
+# 3.2.12
+sub_control_id = "#{control_id}.12"
+control "cis-gke-#{sub_control_id}-#{control_abbrev}" do
+  impact 'medium'
+
+  title "[#{control_abbrev.upcase}] Ensure that the RotateKubeletServerCertificate argument is set to true"
+
+  desc 'Enable kubelet server certificate rotation.'
+  desc 'rationale', "RotateKubeletServerCertificate causes the kubelet to both request a serving certificate
+  after bootstrapping its client credentials and rotate the certificate as its existing credentials
+  expire. This automated periodic rotation ensures that the there are no downtimes due to
+  expired certificates and thus addressing availability in the CIA security triad."
+
+  tag cis_scored: true
+  tag cis_level: 1
+  tag cis_gke: sub_control_id.to_s
+  tag cis_version: cis_version.to_s
+  tag project: gcp_project_id.to_s
+
+  ref 'CIS Benchmark', url: cis_url.to_s
+  ref 'GCP Docs', url: 'https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet-tls-bootstrapping/#kubelet-configuration'
+
+  describe "[#{gcp_project_id}] Kubelet config file #{kubelet_config_file_path}" do
+    subject { kubelet_config_file }
+    its('RotateKubeletServerCertificate') { should cmp 'true' }
+  end
+end
---
+++ run_profiles.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# INSTRUCTIONS:
+# =============
+# To see usage, execute `run_profiles.sh -h`
+
+usage() {
+	echo "Usage: run_profiles.sh [-BbfePh] -c cluster_name -u username -k keyfile -l location"
+	echo 
+	echo "Runs all InSpec profiles of the GKE CIS benchmark"
+	echo
+	echo "  -c cluster_name         Name of the GKE cluster which will be scanned (required)"
+	echo "  -u username             User which logs in to cluster nodes (required). This user requires root privileges on cluster nodes."
+	echo "  -k keyfile              Path to SSH Keyfile required for user to log in to cluster nodes. (required)"
+	echo "  -z zone                 Zone of zonal GKE cluster. Provide either zone or region."
+  echo "  -r region               Region of regional GKE cluster. Provide either zone or region."
+}
+
+main() {
+
+  mkdir -p reports
+
+  echo "Running InSpec profile inspec-gke-cis-gcp ..."
+  inspec exec inspec-gke-cis-gcp -t gcp:// --input-file inputs.yml --reporter cli json:reports/inspec-gke-cis-gcp_report.json html:reports/inspec-gke-cis-gcp_report.html
+  echo "Stored report in reports/inspec-gke-cis-gcp_report."
+
+  echo "Running InSpec profile inspec-gke-cis-k8s ..."
+  inspec exec inspec-gke-cis-k8s -t k8s:// --input-file inputs.yml --reporter cli json:reports/inspec-gke-cis-k8s_report.json html:reports/inspec-gke-cis-k8s_report.html
+  echo "Stored report in reports/inspec-gke-cis-gcp_report."
+
+  if [ -z "$region" ];then
+    location_option="--zone $zone"
+  else
+    location_option="--region $region"
+  fi
+  instance_group_urls=`gcloud container node-pools describe default-pool --cluster inspec-cluster $location_option --format json | jq .instanceGroupUrls | jq @sh`
+
+  for i in "${instance_group_urls[@]}"
+  do
+    instance_group_uri=`echo $i | tr -d \'\"`
+    instance_uri_list=`gcloud compute instance-groups managed list-instances --uri $instance_group_uri`
+    for instance in $instance_uri_list
+    do
+      instance=`echo $instance | sed 's/^.*instances\///'`
+      proxy_command=`gcloud compute ssh $instance --tunnel-through-iap --dry-run $location_option | sed 's/^.*\(ProxyCommand .* -o ProxyUse\).*$/\1/' | sed 's/\ProxyCommand //g' | sed 's/\ -o ProxyUse//g'`
+
+      echo "Running InSpec profile inspec-gke-cis-ssh on node $instance ..."
+      inspec exec inspec-gke-cis-ssh -t ssh://$instance --input-file inputs.yml --proxy_command="$proxy_command" -i $keyfile --user $username --sudo --reporter cli json:reports/inspec-gke-cis-ssh_${instance}_report.json html:reports/inspec-gke-cis-ssh_${instance}_report.html
+      echo "Stored report in reports/inspec-gke-cis-ssh_${instance}_report."
+    done
+  done
+}
+
+while getopts 'c:u:k:z:r' c
+do
+	case $c in
+		c) cluster_name="$OPTARG";;
+		u) username="$OPTARG";;
+		k) keyfile="$OPTARG";;
+		z) zone="$OPTARG";;
+		r) region="$OPTARG";;
+		h|?)
+		  usage
+		  exit 2
+		  ;;
+	esac
+done
+
+main
